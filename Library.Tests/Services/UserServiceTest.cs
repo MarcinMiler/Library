0a79b23df14acd39b69661fb55b8f2258a6c0f0b
Marcin Miler <marcin4951@gmail.com>
2019-02-05 23:28:54 +0100

    Login tests

--- Library.Tests/Services/UserServiceTest.cs
+++ Library.Tests/Services/UserServiceTest.cs
@@ -6,23 +6,12 @@ using Library.Core.Respository;
 using Library.Infrastructure.Services;
 using System.Threading.Tasks;
 using Library.Core.Domain;
+using FluentAssertions;
 
 namespace Library.Tests
 {
     public class UserServiceTest
     {
-        [Fact]
-        public async Task should_register_user()
-        {
-            var userRepositoryMock = new Mock<IUserRepository>();
-            var mapperMock = new Mock<IMapper>();
-            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
-
-            await userService.Register("user@email.com", "password", "Username");
-
-            userRepositoryMock.Verify(x => x.Add(It.IsAny<User>()), Times.Once);
-        }
-
         [Fact]
         public async Task should_return_user_from_given_email()
         {
@@ -37,6 +26,50 @@ namespace Library.Tests
             userRepositoryMock.Setup(x => x.Get(It.IsAny<string>())).ReturnsAsync(() => user);
 
             userRepositoryMock.Verify(x => x.Get(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task should_register_user()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+
+            await userService.Register("user@email.com", "password", "Username");
+
+            userRepositoryMock.Verify(x => x.Add(It.IsAny<User>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task should_login_with_correct_credentials()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+
+            var user = new User("m@m.com", "mm", "mmmmm", "marcinek");
+            userRepositoryMock.Setup(x => x.Get(It.IsAny<string>())).ReturnsAsync(() => user);
+
+            await userService.Login("m@m.com", "mm");
+        }
+
+        [Fact]
+        public async Task should_not_login_with_uncorrect_credentials()
+        {
+            //assert
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var userService = new UserService(userRepositoryMock.Object, mapperMock.Object);
+
+            var user = new User("m@m.com", "mm", "mmmmm", "marcinek");
+            userRepositoryMock.Setup(x => x.Get(It.IsAny<string>())).ReturnsAsync(() => user);
+
+            // act
+            var task = Task.Run(async () => await userService.Login("a@a.com", "mmmmmmm"));
+            var ex = Record.ExceptionAsync(async () => await task);
+
+            ex.Should().NotBeNull();
+            ex.Result.Message.Should().Be("Invalid Credentials.");
         }
     }
 }
